460b27e84c4860d4ecc6b11d41a5bd84444d4521
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2022-12-02 06:41:41 +0100

    Day 3 preparation

---
+++ src/day3.rs
@@ -0,0 +1,5 @@
+use crate::util::*;
+
+pub fn day3(filename: &str) {
+    let input = read_file(filename);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod day1;
 mod day2;
+mod day3;
 mod util;
 
 use util::*;
 
 fn main() {
     day1::day1();
-    day2::day2();
+    println!("Day 2: {:?}", day2::day2());
+    day3::day3("day3-test.txt");
+    day3::day3("day3.txt");
 }
